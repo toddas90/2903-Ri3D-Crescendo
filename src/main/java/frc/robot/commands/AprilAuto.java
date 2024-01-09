1ffdbcab8573d61b71b78d53f123d3dfb8761ea0
Jakob Stickles <jnlstickles@yahoo.com>
2024-01-09 00:53:48 -0800

    Added vision telemetry

--- src/main/java/frc/robot/commands/AprilAuto.java
+++ src/main/java/frc/robot/commands/AprilAuto.java
@@ -4,23 +4,22 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
 public class AprilAuto extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  //private final ExampleSubsystem m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public AprilAuto(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public AprilAuto(){//ExampleSubsystem subsystem) {
+    //m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    //addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,6 +4,11 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.VisionNetParse.AprilTagData;
 import frc.robot.subsystems.VisionNetParse.NoteData;
@@ -23,12 +28,45 @@ public class VisionSubsystem extends SubsystemBase {
   }
 
   // Note: if the AprilTag is not visible, the transform will be null
-  public AprilTagData getAprilTagPosition(int aprilIndex) {
+  public AprilTagData getAprilTag(int aprilIndex) {
     return parser.curAprilTagData[aprilIndex];
   }
 
+  public Set<Integer> getVisibleAprilTags() {
+    Set<Integer> tags = new HashSet<Integer>();
+    for (AprilTagData dat : parser.curAprilTagData) {
+      if (dat.transform != null) {
+        tags.add(dat.id);
+      }
+    }
+    return tags;
+  }
+
   // Note: if no note is visible, the bounds will be null
-  public NoteData getClosestNote(int aprilIndex) {
+  public NoteData getClosestNote() {
     return parser.curNoteData;
+  }
+
+  @Override
+  public void periodic() {
+    // AprilTag telemetry (x points right, y points up, z points in)
+    Set<Integer> aprilTagIds = getVisibleAprilTags();
+    String[] tagInfo = new String[aprilTagIds.size()];
+    int i = 0;
+    for (int id : aprilTagIds) {
+      Transform3d data = getAprilTag(id).transform;
+      tagInfo[i] = String.format("ID %d: x=%.4f, y=%.4f, z=%.4f, rot=%.4f",
+        id, data.getX(), data.getY(), data.getZ(), data.getRotation().getY());
+    }
+    SmartDashboard.putStringArray("AprilTag Locations", tagInfo);
+
+    // Note telemetry
+    NoteData noteDat = getClosestNote();
+    boolean noteVisible = noteDat.bounds != null;
+    SmartDashboard.putBoolean("Note Is Visible", noteVisible);
+    if (noteVisible) {
+      SmartDashboard.putNumber("Note X", noteDat.bounds.x + noteDat.bounds.width / 2);
+      SmartDashboard.putNumber("Note Y", noteDat.bounds.y + noteDat.bounds.height / 2);
+    }
   }
 }
